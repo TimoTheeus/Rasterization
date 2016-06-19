efba4c9f0c3c2cdb81abf96efd0f9c7fd4210ced
Mees <meesvanwinkoop@hotmail.nl>
2016-06-19 20:36:48 +0200

    bewegende cam

--- game.cs
+++ game.cs
@@ -1,48 +1,91 @@
 ﻿using System.Diagnostics;
 using OpenTK;
 using System;
+using OpenTK.Input;
 // minimal OpenTK rendering framework for UU/INFOGR
 // Jacco Bikker, 2016
 
-namespace Template_P3 {
-
-class Game
+namespace Template_P3
 {
-	// member variables
-	public Surface screen;					// background surface for printing etc.
-	Mesh mesh, floor;						// a mesh to draw using OpenGL
-	const float PI = 3.1415926535f;			// PI
-	float a = 0;							// teapot rotation angle
-	Stopwatch timer;						// timer for measuring frame duration
-	Shader shader;							// shader to use for rendering
-	Texture wood;							// texture to use for rendering
-        SceneGraph root,root2;
 
-	// initialize
-	public void Init()
-	{
-		// load teapot
-		mesh = new Mesh( "../../assets/teapot.obj" );
-		floor = new Mesh( "../../assets/floor.obj" );
-		// initialize stopwatch
-		timer = new Stopwatch();
-		timer.Reset();
-		timer.Start();
-		// create shaders
-		shader = new Shader( "../../shaders/vs.glsl", "../../shaders/fs.glsl" );
-		// load a texture
-		wood = new Texture( "../../assets/wood.jpg" );
+    class Game
+    {
+        // member variables
+        public Surface screen;                  // background surface for printing etc.
+        Mesh mesh, floor;                       // a mesh to draw using OpenGL
+        const float PI = 3.1415926535f;         // PI
+        float a = 0;                            // teapot rotation angle
+        Stopwatch timer;                        // timer for measuring frame duration
+        Shader shader;                          // shader to use for rendering
+        Texture wood;							// texture to use for rendering
+        SceneGraph root, root2;
+        Matrix4 camtransMatrix;
+        Matrix4 camrotMatrixX, camrotMatrixY;
+        Vector3 camPos;
+        Vector3 camRot;
+
+        // initialize
+        public void Init()
+        {
+            // load teapot
+            mesh = new Mesh("../../assets/teapot.obj");
+            floor = new Mesh("../../assets/floor.obj");
+            // initialize stopwatch
+            timer = new Stopwatch();
+            timer.Reset();
+            timer.Start();
+            // create shaders
+            shader = new Shader("../../shaders/vs.glsl", "../../shaders/fs.glsl");
+            // load a texture
+            wood = new Texture("../../assets/wood.jpg");
             root = new SceneGraph();
             root2 = new SceneGraph();
             root.AddChildNode(mesh);
             mesh.AddChildNode(floor);
         }
 
-	// tick for background surface
-	public void Tick()
-	{
-		screen.Clear( 0 );
-	}
+        // tick for background surface
+        public void Tick()
+        {
+            screen.Clear(0);
+            if (Keyboard.GetState().IsKeyDown(Key.S))
+            {
+                camPos.Z -= 0.01f * timer.ElapsedMilliseconds * (float)Math.Cos(camRot.Y) * (float)Math.Cos(camRot.X);
+                camPos.X -= 0.01f * timer.ElapsedMilliseconds * (float)Math.Sin(camRot.Y) * -(float)Math.Cos(camRot.X);
+                camPos.Y -= 0.01f * timer.ElapsedMilliseconds * (float)Math.Sin(camRot.X);
+
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.W))
+            {
+                camPos.Z += 0.01f * timer.ElapsedMilliseconds * (float)Math.Cos(camRot.Y) * (float)Math.Cos(camRot.X);
+                camPos.X += 0.01f * timer.ElapsedMilliseconds * (float)Math.Sin(camRot.Y) * -(float)Math.Cos(camRot.X);
+                camPos.Y += 0.01f * timer.ElapsedMilliseconds * (float)Math.Sin(camRot.X);
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.A))
+            {
+                camPos.X += 0.01f * timer.ElapsedMilliseconds * (float)Math.Cos(camRot.Y) * (float)Math.Cos(camRot.X);
+                camPos.Z += 0.01f * timer.ElapsedMilliseconds * (float)Math.Sin(camRot.Y) * (float)Math.Cos(camRot.X);
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.D))
+            {
+                camPos.X -= 0.01f * timer.ElapsedMilliseconds * (float)Math.Cos(camRot.Y) * (float)Math.Cos(camRot.X);
+                camPos.Z -= 0.01f * timer.ElapsedMilliseconds * (float)Math.Sin(camRot.Y) * (float)Math.Cos(camRot.X);
+            }
+            camtransMatrix = Matrix4.CreateTranslation(0 + camPos.X, -4 + camPos.Y, -15 + camPos.Z);
+
+
+            if (Keyboard.GetState().IsKeyDown(Key.Left))
+                camRot.Y -= 0.01f;
+            if (Keyboard.GetState().IsKeyDown(Key.Right))
+                camRot.Y += 0.01f;
+            if (Keyboard.GetState().IsKeyDown(Key.Up))
+                camRot.X -= 0.01f;
+            if (Keyboard.GetState().IsKeyDown(Key.Down))
+                camRot.X += 0.01f;
+
+            camrotMatrixY = Matrix4.CreateRotationY(camRot.Y);
+            camrotMatrixX = Matrix4.CreateRotationX(camRot.X);
+        }
 
         // tick for OpenGL rendering code
         public void RenderGL()
@@ -55,7 +98,9 @@ class Game
             // prepare matrix for rotate
             Matrix4 transform = Matrix4.CreateFromAxisAngle(new Vector3(0, 1, 0), a);
             //camera thingy
-            transform *= Matrix4.CreateTranslation(0, -4, -15);
+            transform *= camtransMatrix;
+            transform *= camrotMatrixY;
+            transform *= camrotMatrixX;
             //fov aspect ratio, near Z plane, far Z plane
             transform *= Matrix4.CreatePerspectiveFieldOfView(1.2f, 1.3f, .1f, 1000f);
             // update rotation
